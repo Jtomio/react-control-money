0765f43f0f59f230f88c4edb29cb04a87ca3c8af
Jeison Tomio <medriase@gmail.com>
2023-03-09 01:20:52 -0300

    finished modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
-import { CloseButton, Content, Overlay } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
 
 export function NewTransactionModal() {
   return (
@@ -17,6 +17,18 @@ export function NewTransactionModal() {
           <input type="text" placeholder='Descrição' required />
           <input type="number" placeholder='Preço' required />
           <input type="text" placeholder='Categoria' required />
+
+          <TransactionType>
+            <TransactionTypeButton variant='income' value='income'>
+              Entrada
+              <ArrowCircleUp size={24} />
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant='outcome' value='outcome'>
+              Saída
+              <ArrowCircleDown size={24} />
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type='submit'>Cadastrar</button>
         </form>
